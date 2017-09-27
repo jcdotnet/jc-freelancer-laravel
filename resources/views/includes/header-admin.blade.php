7e5dfa25cfdb064a1fb95369dd7d6ae24338268e
JC <jcdotnet@hotmail.com>
2017-09-27 17:59:31 +0200

    updated toggler button

--- resources/views/includes/header-admin.blade.php
+++ resources/views/includes/header-admin.blade.php
@@ -1,8 +1,8 @@
 <header>
 	<nav class="navbar navbar-toggleable-sm fixed-top">
 		<div class="container">
-			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar-items" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
+			<button id="jc-toggler" class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar-items" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="fa fa-bars"></span>
 			</button>
 			
 			<a  href="{{ route('admin') }}" class="navbar-brand" >ADMIN AREA</a>
