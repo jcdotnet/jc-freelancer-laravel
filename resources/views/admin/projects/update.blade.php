68f844d5a1330db904181f327ce6f8ebfcc1fdf6
JC <jcdotnet@hotmail.com>
2018-01-12 21:05:09 +0100

    Added media support to TinyMCE

--- resources/views/admin/projects/update.blade.php
+++ resources/views/admin/projects/update.blade.php
@@ -8,7 +8,29 @@
 			branding: false,
 			plugins: 'lists link code image textcolor emoticons',
 		    menubar: false,
-			toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify |  | bullist numlist outdent indent | link image | forecolor backcolor emoticons | code'
+			toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify |  | bullist numlist outdent indent | link image | forecolor backcolor emoticons | code',
+			relative_urls: false,
+			file_browser_callback : function(field_name, url, type, win) {
+				var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+				var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+				
+				var cmsURL = '/freelancer-laravel/public/media?field_name=' + field_name; // dev
+				//var cmsURL = '/media?field_name=' + field_name; // prod 
+				if (type == 'image') {
+					cmsURL = cmsURL + "&type=Images";
+				} else {
+					cmsURL = cmsURL + "&type=Files";
+				}
+
+				tinyMCE.activeEditor.windowManager.open({
+					file : cmsURL,
+					title : 'Filemanager',
+					width : x * 0.8,
+					height : y * 0.8,
+					resizable : "yes",
+					close_previous : "no"
+				});
+			}
 		});
 	</script>
 @endsection
